4fd2aaa1d2d414221503b75ae07fd2dc66b0bc42
Hp <Hp@DESKTOP-3R37S68>
2017-02-08 17:32:14 +0530

    printed bro's name

--- src/testofgit/TestOfGIt.java
+++ src/testofgit/TestOfGIt.java
@@ -17,6 +17,7 @@ public class TestOfGIt {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("sunny prasad");
+        System.out.println("Vishal");
     }
     
 }
